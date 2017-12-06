83b23bc80f732d03b61ab8890c5e9c7f526bc6e5
pujianguo <jianguo@jianguo.com>
2017-12-06 22:40:36 +0800

    add file

---
+++ src/components/form/ComA.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		{{param}}
+	</div>
+</template>
+
+<script>
+export default{
+	props: {
+		'param': {
+			type: String,
+			// required: true
+			default: 'i am children component default data'
+		}
+	}
+}
+</script>
---
+++ src/components/form/Event.vue
@@ -0,0 +1,51 @@
+<template>
+    <div name="event">
+        <p> click: <Button type="primary" @click="onClick">click</Button> </p>
+        <p> keydown: <input type="text" name="name" @keydown="onKeyDown"> </p>
+        <p> keydown.enter: <input type="text" name="name" @keydown.enter="onKeyDown"> </p>
+        <p :is="comToRender" :param="param"></p>
+    </div>
+</template>
+
+<script>
+import ComA from './ComA'
+
+let vm = null
+
+export default {
+  name: 'event',
+  components: {
+    ComA
+  },
+  data () {
+    return {
+      param: 'i am parent component data',
+      comToRender: 'ComA'
+    }
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+    onClick (event) {
+      console.log(event)
+    },
+    onKeyDown (event) {
+      console.log(event)
+      console.log(event.key)
+      console.log(event.keyCode)
+    },
+    onKeyUp (event) {
+      console.log(event)
+    }
+  },
+  mounted () {
+    vm = this
+    console.log(vm)
+  }
+}
+</script>
+
+<style scoped>
+</style>
