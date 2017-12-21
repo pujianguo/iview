4703627d093ef5e432d7265765f38039f9b7b8b2
pujianguo <jianguo@jianguo.com>
2017-12-22 01:41:50 +0800

    add basetable file

--- src/components/table/BaseTable.vue
+++ src/components/table/BaseTable.vue
@@ -10,31 +10,201 @@
                 刷新
             </a>
             <Alert>
-              简介
+              简介: 
+              <ul>
+                <li>1.通过插槽来实现添加、删除按钮在导航栏的显示</li>
+                <li>2.显示复选框组件，通过selectionChange来监听iview的Table组件中的选中方法，返回选中项</li>
+              </ul>
             </Alert>
-
-            <p>content</p>
+            <NTable :nColumns="usertListColumns" :nData="userListTableData" :page="5" @selectionChange="selectionChange">
+              <span slot="handleButtons">
+                <Button type="info" @click="addHandle">添加</Button>
+                <Button type="error" @click="batchDeleteHandle" :disabled="selectedCount === 0" :loading="batchDeleteBtnLoading">删除<span v-if="selectedCount > 0"> ({{selectedCount}}) </span></Button>
+              </span>
+            </NTable>
         </Card>
     </div>
 </template>
 
 <script>
+import {jsonData} from './list'
+import NTable from './NTable'
 let vm = null
+
 export default {
   name: 'basetable',
+  components: {
+    NTable
+  },
   data () {
     return {
+      selectedList: [], // 列表中选中的项
+      batchDeleteBtnLoading: false,
+      userListTableData: [],
+      usertListColumns: [
+        // {
+        //   title: '',
+        //   key: 'checked',
+        //   render: (h, params) => {
+        //     return h('Checkbox', {
+        //       props: {
+        //         value: this.selected.indexOf(params.row.index) >= 0
+        //       },
+        //       on: {
+        //         'on-change': (value) => {
+        //           if (value) {
+        //             this.selected.push(params.row.index)
+        //           } else {
+        //             let i = this.selected.indexOf(params.row.index)
+        //             if (i >= 0) {
+        //               this.selected.splice(i, 1)
+        //             }
+        //           }
+        //         }
+        //       }
+        //     })
+        //   }
+        // },
+        {
+          type: 'selection',
+          width: 60,
+          align: 'center'
+        },
+        {
+          title: 'index',
+          render: (h, params) => {
+            return params.row.index
+          }
+        },
+        {
+          title: '姓名',
+          key: 'name',
+          render: (h, params) => {
+            return params.row.name
+          }
+        },
+        {
+          title: '性别',
+          key: 'sex',
+          render: (h, params) => {
+            return params.row.sex
+          }
+        },
+        {
+          title: '年龄',
+          key: 'addr',
+          render: (h, params) => {
+            return params.row.age
+          }
+        }, 
+        {
+          title: '生日',
+          key: 'addr',
+          render: (h, params) => {
+            return params.row.birth
+          }
+        },
+        {
+          title: '地址',
+          key: 'addr',
+          render: (h, params) => {
+            return params.row.addr
+          }
+        },
+        {
+          title: '操作',
+          key: 'option',
+          render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.editHandle(params.row.index)
+                  }
+                }
+              }, '编辑'),
+              h('Button', {
+                props: {
+                  type: 'error',
+                  size: 'small'
+                },
+                on: {
+                  click: () => {
+                    this.deleteHandle(params.row.index)
+                  }
+                }
+              }, '删除')
+            ])
+          }
+        }
+      ]
     }
   },
   computed: {
+    selectedCount () {
+      return this.selectedList.length
+    }
   },
   watch: {
   },
   methods: {
+    initData () {
+      this.userListTableData = jsonData.map((x, i) => {
+        x.index = i
+        return  x
+      })
+    },
+    selectionChange (val) {
+      this.selectedList = val
+    },
+    addHandle () {
+      console.log('add')
+    },
+    editHandle (index) {
+      let data = jsonData[index]
+      console.log(data)
+    },
+    deleteHandle (index) {
+      let data = jsonData[index]
+      console.log(data)
+      this.$Modal.confirm({
+        title: '确认删除',
+        content: `确认删除该记录吗？`,
+        onOk: () => {
+          this.$Message.info('删除')
+        },
+        onCancel: () => {
+          this.$Message.info('取消删除')
+        }
+      })
+    },
+    batchDeleteHandle () {
+      var ids = this.selectedList.map(item => item.id).toString()
+      this.$Modal.confirm({
+        title: '确认删除',
+        content: `确认删除选中的 ${this.selectedCount} 条记录吗？`,
+        onOk: () => {
+          this.batchDeleteBtnLoading = true
+          // async 操作
+          this.batchDeleteBtnLoading = false
+          this.$Message.info('删除')
+        },
+        onCancel: () => {
+          this.$Message.info('取消删除')
+        }
+      })
+    }
   },
   mounted () {
     vm = this
-    console.log(vm)
+    this.initData()
   }
 }
 </script>
---
+++ src/components/table/NTable.vue
@@ -0,0 +1,163 @@
+<template>
+  <div name="n_table">
+      <div style="padding-bottom: 10px; overflow: hidden">
+        <div v-if="hasSearch" style="float: left;">
+          <Input v-model.trim="nSearchVal" @on-change="search">
+            <Button slot="append" icon="ios-search"></Button>
+          </Input>
+        </div>
+        <div style="float: right;">
+          <slot name="handleButtons"></slot>
+          <Button type="primary" v-if="hasExportData" @click="exportData()">导出数据</Button>
+          <Poptip v-if="hasDefaultColumn" placement="bottom-end">
+            <Button>自定义列</Button>
+            <div class="api" slot="content">
+              <ul>
+                <li v-for="(c, i) in nColumns" v-if="i > 0" :key="i">
+                  <Checkbox :value="nColumnsExcept.indexOf(c.key) === -1" @on-change="columnsExcept(c.key)">{{ c.title }}</Checkbox>
+                </li>
+              </ul>
+            </div>
+          </Poptip>
+        </div>
+      </div>
+      <Table ref="nTable" class="ivu-table-auto" border :columns="filterColumns" :data="nPageData" @on-selection-change="selectionChange"></Table>
+      <div v-if="hasPage" style="margin: 10px;overflow: hidden">
+        <div style="float: right;">
+          <Page
+            :total="nFilterData.length"
+            :current="nPage.current"
+            :page-size="nPage.size"
+            @on-change="changePage"
+            show-total
+            >
+          </Page>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { nCopy } from '@/utils/help'
+
+export default {
+  props: {
+    nColumns: {
+      type: Array,
+      required: true
+    },
+    nData: {
+      type: Array,
+      required: true
+    },
+    page: {
+      type: Number,
+      default: 10
+    },
+    hasSearch: {
+      type: Boolean,
+      default: true
+    },
+    hasExportData: {
+      type: Boolean,
+      default: true
+    },
+    hasDefaultColumn: {
+      type: Boolean,
+      default: true
+    },
+    hasPage: {
+      type: Boolean,
+      default: true
+    }
+  },
+  watch: {
+    nData () {
+      this.nInit()
+    },
+    currentPage () {
+      this.getPageData()
+    }
+  },
+  computed: {
+    filterColumns () {
+      return this.nColumns.filter(x => {
+        return this.nColumnsExcept.indexOf(x.key) === -1
+      })
+    },
+    currentPage () {
+      return this.nPage.current
+    }
+  },
+  data () {
+    return {
+      nPage: {
+        current: 1,
+        size: this.page
+      },
+      nColumnsExcept: [],
+      nSearchVal: '',
+      nFilterData: [],
+      nPageData: []
+    }
+  },
+  methods: {
+    nInit () {
+      this.nPage.current = 1
+      this.getPageData()
+    },
+    exportData () {
+      this.$refs.nTable.exportCsv({
+        filename: 'table'
+      })
+    },
+    columnsExcept (key) {
+      let index = this.nColumnsExcept.indexOf(key)
+      if (index === -1) {
+        this.nColumnsExcept.push(key)
+      } else {
+        this.nColumnsExcept.splice(index, 1)
+      }
+    },
+    changePage (page) {
+      this.nPage.current = page
+    },
+    search () {
+      this.nPage.current = 1
+      this.getPageData()
+    },
+    getFilterData () {
+      let searchVal = this.nSearchVal
+      let data = nCopy(this.nData)
+      if (this.nSearchVal) {
+        let ret = []
+        data.map(x => {
+          for (let i in this.nColumns) {
+            let key = this.nColumns[i].key
+            if (x[key] && (x[key] + '').indexOf(searchVal) >= 0) {
+              ret.push(x)
+              break
+            }
+          }
+        })
+        return ret
+      } 
+      return data
+    },
+    getPageData () {
+      this.nFilterData = this.getFilterData()
+      let data = nCopy(this.nFilterData)
+      this.nPageData = data.splice((this.nPage.current - 1) * this.nPage.size, this.nPage.size)
+    },
+    selectionChange (val) {
+      this.$emit('selectionChange', val)
+    }
+  },
+  created () {
+    this.nInit()
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
